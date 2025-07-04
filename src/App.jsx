a1c91327c65b2d8cb6125dd53cb1472320c9e2e6
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2025-07-04 13:15:26 -0500

    Initial Vite and MUI Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Button variant="contained">Contained</Button>
+    </>
+  );
+}
+
+export default App;
